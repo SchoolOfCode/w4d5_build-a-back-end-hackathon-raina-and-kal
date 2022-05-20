ff8b8791f1a9641270f37305abb0091ee00bb410
chocorainaaa <blackettraina@gmail.com>
2022-05-20 11:19:37 +0100

    issues with export import

--- app.js
+++ app.js
@@ -8,10 +8,17 @@ const PORT = 3000;
 app.use(express.static("public"));
 app.use(express.json());
 
+// route to recipes.js
+app.use("/", function (req, res, next){
+  console.log(req.body);
+  next();
+})
+
+
 
 // route to recipes.js
-
-
+import recipesRouter from  = require("/router/recipes.js");
+app.use("/recipes", recipesRouter);
 
 
 
--- router/recipes.js
+++ router/recipes.js
@@ -1,3 +1,21 @@
-//Write your router code here!
+import express from "express";
+const router = express.Router();
+import recipes from "../libs/recipes-data.js"
+
+// Get request by ID 
+router.get("/:id", function (req, res) {
+    let id =  Number(req.params.id);  // Need to convert to a number
+    console.log(id);
+    console.log(recipes);
+    for (i = 0; i < recipes.length; i++) {
+
+    }
+
+
+})
+
+
+
+
 
 module.exports = recipesRouter;
