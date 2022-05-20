d83376c299d39e7f8f3ada785230fda5289da90b
kalhollywood <kal.hollywood@gmail.com>
2022-05-20 11:53:20 +0100

    GET, DEFAULT and POST completed

--- app.js
+++ app.js
@@ -9,19 +9,14 @@ app.use(express.static("public"));
 app.use(express.json());
 
 // route to recipes.js
-app.use("/", function (req, res, next){
+app.use("/", function (req, res, next) {
   console.log(req.body);
   next();
-})
-
-
+});
 
 // route to recipes.js
-import recipesRouter from  = require("/router/recipes.js");
+import { recipesRouter } from "./router/recipes.js";
 app.use("/recipes", recipesRouter);
-
-
-
 
 /* DO NOT CHANGE THIS ROUTE - it serves our front-end */
 app.get("/", function (req, res) {
--- router/recipes.js
+++ router/recipes.js
@@ -1,21 +1,45 @@
 import express from "express";
 const router = express.Router();
-import recipes from "../libs/recipes-data.js"
+import recipes from "../libs/recipes-data.js";
 
-// Get request by ID 
+// Get request by ID
 router.get("/:id", function (req, res) {
-    let id =  Number(req.params.id);  // Need to convert to a number
-    console.log(id);
-    console.log(recipes);
-    for (i = 0; i < recipes.length; i++) {
-
+  let id = Number(req.params.id); // Need to convert to a number
+  for (let i = 0; i < recipes.length; i++) {
+    if (id === recipes[i].id) {
+      const responseObject = {
+        success: "true",
+        message: `This is recipe with ID: ${id}`,
+        data: recipes[i],
+      };
+      res.json(responseObject);
     }
+  }
+});
 
+// Set up POST handler
 
-})
+router.post("/", function (req, res) {
+  let userInput = req.body;
+  recipes.push(userInput);
+  console.log(recipes);
+  const responseObject = {
+    success: "true",
+    message: `This is the new list of recipes`,
+    data: recipes,
+  };
+  res.json(responseObject);
+});
 
+// Set up default handler for /recipes
 
+router.get("/", function (req, res) {
+  const responseObject = {
+    success: "true",
+    message: `This is all of the recipes`,
+    data: recipes,
+  };
+  res.json(responseObject);
+});
 
-
-
-module.exports = recipesRouter;
+export { router as recipesRouter };
